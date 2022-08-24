c23b9056211edcd85ae93e26806d9dc0ab78eebb
priyabecse10 <103168941+priyabecse10@users.noreply.github.com>
2022-08-24 20:46:58 +0530

    Update file.js

--- file.js
+++ file.js
@@ -2,28 +2,28 @@ var fs=require('fs');
 //synchronous method
 var readMe=fs.readFileSync('text.xml');
 
-console.log(readMe.toString());
+console.log(readMe.toString().split());
 // var parser=new DOMParser();
 // var xmlDoc=parser.parseFromString(xml,'text/xml');
 // console.log(xmlDoc.getElementsByTagName('xml'));
-//  const readLastLines=require('read-last-lines');
-//  readLastLines.read('text.xml',5)
-//  .then((lines)=>console.log(lines));
+const readLastLines=require('read-last-lines');
+readLastLines.read('text.xml',5)
+.then((lines)=>console.log(lines));
 
 
 // // Asynchronous method
-// fs.readFile('text.xml',function(err,readMe)
-// {
-//     if(err)
-//     {
-//         return console.log("Error");
-//     }
-//     console.log(readMe.toString());
-// });
+ fs.readFile('text.xml',function(err,readMe)
+ {
+     if(err)
+     {
+         return console.log("Error");
+     }
+     console.log(readMe.toString());
+ });
 
-// fs.writeFile('writeFile.txt',readMe, err=>{
-//     if(err){
-//         console.error(err)
-//         return
-//     }
-// });
+ fs.writeFile('writeFile.txt',readMe, err=>{
+    if(err){
+         console.error(err)
+         return
+    }
+ });

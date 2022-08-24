1003e6b2426a16dae7123650aa28c19dd32ac32d
priyabecse10 <103168941+priyabecse10@users.noreply.github.com>
2022-08-24 20:38:17 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,29 @@
+var fs=require('fs');
+//synchronous method
+var readMe=fs.readFileSync('text.xml');
+
+console.log(readMe.toString());
+// var parser=new DOMParser();
+// var xmlDoc=parser.parseFromString(xml,'text/xml');
+// console.log(xmlDoc.getElementsByTagName('xml'));
+//  const readLastLines=require('read-last-lines');
+//  readLastLines.read('text.xml',5)
+//  .then((lines)=>console.log(lines));
+
+
+// // Asynchronous method
+// fs.readFile('text.xml',function(err,readMe)
+// {
+//     if(err)
+//     {
+//         return console.log("Error");
+//     }
+//     console.log(readMe.toString());
+// });
+
+// fs.writeFile('writeFile.txt',readMe, err=>{
+//     if(err){
+//         console.error(err)
+//         return
+//     }
+// });
